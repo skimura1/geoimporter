64963887b37838174ac3ce674947588374149700
Skyler Kimura <skyler8@hawaii.edu>
2023-05-15 08:14:31 -1000

    separate geoserver upload functions in different file

--- geoserver-rest.py
+++ geoserver-rest.py
@@ -1,72 +1,76 @@
-import os
 import re
-
 import geopandas as gpd
-import sqlalchemy as sa
-from geo.Geoserver import Geoserver
 
-# Variables
-geo_user = 'INSERT GEO USERNAME'
-geo_pass = 'INSERT GEO PASS'
-geo_host = 'INSERT GEO HOSTNAME'
-geo_workspace = 'INSERT GEO WORKSPACE'
-geo_storename = 'INSERT GEO STORENAME'
-pg_user = 'INSERT PG USERNAME'
-pg_passw = 'INSERT PG PASSWORD'
-pg_host = 'INSERT PG HOST IP ADDRESS'
-pg_port = 'INSERT PG PORT'
-pg_db = 'INSERT PG DB'
-
-
-# Create connection to Geoserver and starting workspace
-geo = Geoserver(geo_host, username=geo_user, password=geo_pass)
-
-geo.create_workspace(workspace=geo_workspace)
-
-# Create connection to the postgresql database
-geo.create_featurestore(store_name=geo_storename, workspace=geo_workspace, db=pg_db, host=pg_host,
-                        port=pg_port, pg_user=pg_user,
-                        pg_password=pg_passw, schema='public')
-
-# Create interface to handle/manage database
-engine = sa.create_engine('postgresql://' + pg_user + ':' + pg_passw + '@' + pg_host + ':' + pg_port + '/' + pg_db)
-insp = sa.inspect(engine)
-
-# Import Shapefiles into the PostgreSQLDB, then publish on Geoserver
-shp_dir = 'SHAPE FILE DIRECTORY'
-errorLayer = []
-for (root, dirs, files) in os.walk(shp_dir):
-    for file in files:
-        filename = file[:-4]
-        if re.search(r'.shp$', file):
-            print('Uploading ' + filename)
-            shp_file = gpd.read_file(root + "/" + file)
-            if not insp.has_table(filename, schema='public'):
+def tiff_walk(geoserver, files, workspace):
+    """
+    Walk through all the directories looking for TIFF/Raster files and uploading to Geoserver
+    :param geoserver: the geoserver that we want to publish the files onto
+    :param tiff_dir: the directory that we are trying to "walk through"
+    :param workspace: the workspace on geoserver that will hold the layer
+    :return: the status of the operation; whether it has failed or succeeded
+    """
+    error_layer = []
+    import_num = 0
+    print("Tiff file importing initialized...")
+    for (root_dir, dirs, files) in os.walk(tiff_dir):
+        for file in files:
+            if re.search(r'.tif$', file):
+                filename = file[:-4]
+                print('Uploading ' + filename)
                 try:
-                    shp_file.to_postgis(filename, engine, index=True, index_label='Index')
+                    geoserver.create_coveragestore(layer_name=filename, path=root_dir + '/' + file, workspace=workspace)
+                    import_num += 1
                 except Exception as e:
-                    errorTuple = (filename, e)
-                    errorLayer.append(filename)
-                    print("Error with " + filename)
+                    error_tuple = (filename, e)
+                    error_layer.append(filename)
+                    print('Error with ' + filename)
                     continue
-            else:
-                print(filename + ' exists already in PostgreSQL database!')
-            geo.publish_featurestore(workspace='CRC', store_name='PUC_SLR_Viewer', pg_table=filename)
-            print(filename + ' upload completed!')
+    status = 'Successfully imported ' + str(import_num) + ' layers!'
+    print(status)
+    if len(error_layer):
+        status += 'There was an error in' + ''.join(error_layer)
+    return status
 
-# Import and publish Raster (TIF Data) onto Geoserver 
-tiff_dir = 'TIFF DIRECTORY'
-for (root, dirs, files) in os.walk(tiff_dir):
-    for file in files:
-        if re.search(r'.tif$', file):
+def upload_raster(file, path, workspace):
+    
+
+
+def shp_walk(geoserver, engine, shp_dir, workspace, storename):
+    """
+    Walk through all the directories looking for shapefiles and uploading them to the PostgreSQL/POSTGIS DB.
+    Then make a connection the geoserver, so the layers on PostgreSQL/POSTGIS DB can be published on Geoserver.
+    :param geoserver: the geoserver that we want to publish the files onto
+    :param engine: the database connection that we want to upload the files from
+    :param shp_dir: the directory that the shapefiles are stored
+    :param workspace: the workspace on geoserver that will hold the layer
+    :param storename: the storename on geoserver that will be basis of connection to POSTGIS DB
+    :return: the status of the operation; whether it has failed or succeeded
+    """
+    error_layer = []
+    import_num = 0
+    insp = sa.inspect(engine)
+    print("Shape file importing initialized...")
+    for (root_dir, dirs, files) in os.walk(shp_dir):
+        for file in files:
             filename = file[:-4]
-            print('Uploading ' + filename)
-            try:
-                geo.create_coveragestore(layer_name=filename, path=root + '/' + file, workspace='CRC')
-            except Exception as e:
-                errorTuple = (filename, e)
-                errorLayer.append(filename)
-                print('Error with ' + filename)
-                continue
+            if re.search(r'.shp$', file):
+                print("Uploading " + filename)
+                shp_file = gpd.read_file(root_dir + "/" + file)
+                if not insp.has_table(filename, schema="public"):
+                    try:
+                        shp_file.to_postgis(filename, engine, index=True, index_label='Index')
+                    except Exception as e:
+                        error_tuple = (filename, e)
+                        error_layer.append(filename)
+                        print("Error with " + filename)
+                        continue
+                else:
+                    print(filename + ' exists already in PostgreSQL database!')
+                if geoserver.publish_featurestore(workspace=workspace, store_name=storename, pg_table=filename) is None:
+                    print(filename + " upload completed to Geoserver!")
+                    import_num += 1
+    status = 'Successfully imported ' + str(import_num) + ' layers!'
+    if len(error_layer):
+        status += "There was an error in" + ''.join(error_layer)
+    return status
 
-print(errorLayer)
