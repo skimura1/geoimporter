63888fd10f8327f69aaaf3c15307f611a4ebd7cc
Skyler Kimura <skyler8@hawaii.edu>
2023-05-15 16:10:39 -1000

    rename geoserver-rest to geoserver_rest

---
+++ geoserver_rest.py
@@ -0,0 +1,40 @@
+import os
+
+import sqlalchemy as sa
+import geopandas as gpd
+
+def upload_raster(geoserver, filepath, workspace):
+    filename = os.path.basename(filepath)
+
+    print("Importing " + filename)
+    try:
+        geoserver.create_coveragestore(layer_name=filename[:-4], path=filepath, workspace=workspace)
+    except Exception as e:
+        print('Error with ' + filename + ' with ' + e)
+        return False
+    return True
+
+def upload_postgis(filepath, engine):
+    filename = os.path.basename(filepath)
+    try:
+        shp_file = gpd.read_file(filepath)
+        insp = sa.inspect(engine)
+        if not insp.has_table(filename[:-4], schema="public"):
+            shp_file.to_postgis(filename[:-4], engine, index=True, index_lable='index')
+        else:
+            print(filename[:-4] + " already exists in PostgreSQL database!")
+    except Exception as e:
+        print("Error uploading shapefile to PostgreSQL database with " + filename + " with " + e)
+        return False
+    print(filename + " uploaded to PostgreSQL database!")
+    return True
+
+def upload_shapefile(geoserver, filepath, workspace, storename):
+    filename = os.path.basename(filepath)
+    print("Uploading " + filename)
+    if geoserver.publish_featurestore(workspace=workspace, store_name=storename, pg_table=filename[:-4]) is not None:
+        print("Something went wrong with uploading Geoserver")
+        return False
+    print(filename + " upload completed to Geoserver!")
+    return True
+
