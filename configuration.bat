77f71fd33dda46ff51291dc06d1bb225715b5c96
Skyler Kimura <skyler8@hawaii.edu>
2024-06-14 10:34:56 -1000

    clean up directories

--- configuration.bat
+++
@@ -1,2 +0,0 @@
-
-pip install geopandas sqlalchemy geoalchemy2 psycopg2 geoserver-rest python-dotenv tk
--- configuration.sh
+++
@@ -1,3 +0,0 @@
-apt-get install python3-tk
-pip install GDAL==$(gdal-config --version | awk -F'[.]' '{print $1"."$2}')
-pip install geopandas sqlalchemy geoalchemy2 psycopg2 geoserver-rest python-dotenv tk
